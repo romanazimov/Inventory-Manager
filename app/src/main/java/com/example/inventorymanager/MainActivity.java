ff5e42e5dbec3c6fb4f6df4cfa88d67466ecfdad
Roman <azimov308@live.com>
2020-04-27 13:52:56 -0400

    Issue save photo
    Problem when trying to save the photo that was taken using the camera.

--- app/src/main/java/com/example/inventorymanager/MainActivity.java
+++ app/src/main/java/com/example/inventorymanager/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +18,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.sql.Date;
 
 import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,14 +27,19 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
 public class MainActivity extends AppCompatActivity {
     public static TextView resultTextView;
     private Button scanButton, firstPictureButton;
-    //private ImageView mImageView;
+    private ImageView imageView;
 
     private static final int CAMERA_REQUEST_CODE = 1;
     private StorageReference mStorageRef;
     private ProgressDialog mProgressRef;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     @Override
@@ -40,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         resultTextView = findViewById(R.id.barcodetextview);
         scanButton = findViewById(R.id.buttonscan);
         firstPictureButton = findViewById(R.id.firstPicButton);
+        imageView = findViewById(R.id.imageView);
 
         mStorageRef = FirebaseStorage.getInstance().getReference();
 
@@ -55,44 +66,65 @@ public class MainActivity extends AppCompatActivity {
         firstPictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try
-                {
-                    Intent intent = new Intent();
-                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent, CAMERA_REQUEST_CODE);
-                }
-                catch (Exception e)
-                {
-                    e.getStackTrace();
-                }
-
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+                dispatchTakePictureIntent();
             }
         });
 
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    String currentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
-            mProgressRef.setMessage("Uploading Image...");
-            mProgressRef.show();
-
-            Uri File = data.getData();
-            //StorageReference filepath = mStorageRef.child("Photos").child(uri.getLastPathSegment());
-            StorageReference filePath = mStorageRef.child("images/item.jpg");
-
-            filePath.putFile(File).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    mProgressRef.dismiss();
-                    Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
-                    while (!urlTask.isSuccessful());
-                    Uri downloadUrl = urlTask.getResult();
-                    Log.i("The URL : ", downloadUrl.toString());
-                }
-            });
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            galleryAddPic();
         }
+
     }
 }
